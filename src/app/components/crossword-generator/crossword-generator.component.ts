6b82465722392f609991ebb54f790d112fe4e23b
Venkateshwaran Selvaraj <venkateshwarans@ilimi.in>
2020-02-17 15:24:16 +0530

    calling conceptnet.io

--- src/app/components/crossword-generator/crossword-generator.component.ts
+++ src/app/components/crossword-generator/crossword-generator.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CrosswordService } from '../../services/crossword.service';
 import { HttpClient } from '@angular/common/http';
+import * as _ from 'lodash-es';
 declare const Crossword: any;
 declare const generate: any;
 
@@ -11,8 +12,27 @@ declare const generate: any;
 })
 export class CrosswordGeneratorComponent implements OnInit {
 
-  private conceptNetAPI = `http://api.conceptnet.io/c/en/limit?rel=/r/Synonym&limit=40`;
+  private relativeParam =  `?rel=/r/UsedFor`;
+  private limit = `&limit=100`;
+  private contributor = '/s/resource/verbosity';
+  private topic = `animal`;
+  private conceptNetAPI = `http://api.conceptnet.io/c/en/${this.topic}${this.relativeParam}${this.limit}`;
+  private relativeParamArray = [
+    '?rel=/r/RelatedTo',
+    '?rel=/r/IsA',
+    '?rel=/r/Synonym',
+    '?rel=/r/CapableOf',
+    '?rel=/r/HasContext',
+    '?rel=/r/ReceivesAction',
+    '?rel=/r/HasProperty',
+    '?rel=/r/UsedFor',
+    '?rel=/r/MannerOf',
+    '?rel=/r/MadeOf',
+    '?rel=/r/DefinedAs',
+    '?rel=/r/AtLocation'
+  ]
 
+  public filteredEdges: any;
   constructor(private http: HttpClient) { }
 
   ngOnInit() {
@@ -23,10 +43,13 @@ export class CrosswordGeneratorComponent implements OnInit {
     generate(Number(val));
     this.http.get(this.conceptNetAPI).subscribe((response: any) => {
       console.log(response.edges);
+     this.filteredEdges =  _.filter(response.edges, (edge) => {
+        return _.find(edge.sources, {'contributor': this.contributor});
+      });
+      console.log(this.filteredEdges);
     }, (error) => {
       console.log(error);
     });
   }
 
-  // /s/resource/verbosity
-
+}
