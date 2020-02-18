ab56de069753f6e8791800f8c5a9d7f5c28db93f
Venkateshwaran Selvaraj <venkateshwarans@ilimi.in>
2020-02-18 06:46:04 +0530

    Words for animals

--- src/app/modules/public/components/crossword-generator/crossword-generator.component.ts
+++ src/app/modules/public/components/crossword-generator/crossword-generator.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Subscription, of, throwError, Observable, Subject } from 'rxjs';
+import { Subscription, of, throwError, Observable, Subject, forkJoin } from 'rxjs';
 import { map, mergeMap } from 'rxjs/operators';
 import * as _ from 'lodash-es';
 declare const Crossword: any;
@@ -17,13 +17,17 @@ export class CrosswordGeneratorComponent implements OnInit {
   private relativeParam =  `?rel=/r/UsedFor`;
   private limit = `&limit=100`;
   private contributor = '/s/resource/verbosity';
+  public category: any;
   private topic = [
-    'animal', 'fruits', 'countries'
+    {type: 'animal', rel: ['/r/UsedFor', '/r/IsA'], sentence: '___ is used for '},
+    {type: 'fruits', rel: '/r/HasA', sentence: '___ has a '},
+    {type: 'country', rel: '/r/dbpedia/capital', sentence: '___  capital is'}
   ];
-  private conceptNetAPI = `http://api.conceptnet.io/c/en/${this.topic[0]}${this.relativeParam}${this.limit}`;
+  private conceptNetAPI = `http://api.conceptnet.io/c/en/${this.topic[0].type}${this.relativeParam}${this.limit}`;
   private relativeParamArray = [
     '?rel=/r/RelatedTo',
     '?rel=/r/IsA',
+    '?rel=/r/HasA',
     '?rel=/r/Synonym',
     '?rel=/r/CapableOf',
     '?rel=/r/HasContext',
@@ -33,52 +37,56 @@ export class CrosswordGeneratorComponent implements OnInit {
     '?rel=/r/MannerOf',
     '?rel=/r/MadeOf',
     '?rel=/r/DefinedAs',
-    '?rel=/r/AtLocation'
+    '?rel=/r/AtLocation',
+    '?rel=/r/dbpedia/capital'
   ];
 
   public filteredEdges: any;
   constructor(private http: HttpClient) { }
 
-  ngOnInit() {}
-
-
-  getRows(val): Observable<any> {
-    this.http.get(this.conceptNetAPI).subscribe((response: any) => {
-      console.log(response.edges);
-      this.filteredEdges =  _.filter(response.edges, (edge) => {
-        if (this.contributor.length > 0) {
-          return _.find(edge.sources, {contributor: this.contributor});
-        } else {
-          return 1;
-        }
-      }).map(e => {
-        return {
-          ['word']: e.start.label,
-          ['clue']: e.end.label};
-      });
-
-      entries = this.filteredEdges;
-      generate(Number(val), entries);
-
-    }, (error) => {
-      console.log(error);
-    });
-
-
-    return this.http.get(this.conceptNetAPI).pipe(
-      mergeMap((entities: any) => {
-        console.log(entities);
-        return entities;
-      }),
-      mergeMap((value: any, index: number) => {
-        console.log(item);
-      })
-
-      ).subscribe(results => {
-      console.log(results);
-      return results;
-    });
+  ngOnInit() {
+    this.category = 'animal';
   }
+
+
+  getRows(val) {
+
+    this.http.get(this.conceptNetAPI).pipe(
+      mergeMap((res: any) => {
+        const results = res.edges;
+
+        // tslint:disable-next-line:max-line-length
+        const request = _.map(results, (result) => this.http.get(`http://api.conceptnet.io/c/en/${result.start.label}${this.relativeParam}${this.limit}`));
+        return forkJoin(request);
+      })
+    ).subscribe((words) => {
+      console.log(words);
+
+      this.filteredEdges = _.filter(words, word => {
+        return word.edges.length > 0;
+      }).map(item => {
+        const firstRel = _.find(this.topic, {type: this.category}).rel[0];
+        const secondRel = _.find(this.topic, {type: this.category}).rel[1];
+        const firstFilter = _.filter(item.edges, i => i.rel['@id'] === firstRel);
+        const secondFilter = _.filter(item.edges, i => i.rel['@id'] === secondRel);
+        if (firstFilter && firstFilter.length > 2) {
+          return {
+              ['word']: firstFilter[1].end.label,
+              ['clue']: firstFilter[1].start.label
+          };
+        } else if (secondFilter && secondFilter.length > 2) {
+          return {
+            ['word']: secondFilter[1].end.label,
+            ['clue']: secondFilter[1].start.label
+          };
+        }
+      });
+      console.log(this.filteredEdges);
+      this.filteredEdges = _.uniqBy(_.compact(this.filteredEdges), 'word');
+      console.log(this.filteredEdges);
+      generate(Number(val), this.filteredEdges);
+    });
+}
 
 
 
--- src/app/modules/public/public.module.ts
+++ src/app/modules/public/public.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 import { LoginComponent, CrosswordGeneratorComponent } from './components';
 import { HttpClientModule } from '@angular/common/http';
 import { PublicRoutingModule } from './public-routing.module';
+import {SuiDropdownModule} from 'ng2-semantic-ui';
 
 @NgModule({
   imports: [
     CommonModule,
     PublicRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    SuiDropdownModule
   ],
   declarations: [LoginComponent, CrosswordGeneratorComponent]
 })
