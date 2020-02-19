c6622c005eeff0e30da931b022f5663062745d70
swetha-aggidevara <swetha@ilimi.in>
2020-02-19 14:51:49 +0530

    added header in content-review

--- src/app/modules/public/public.module.ts
+++ src/app/modules/public/public.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent, CrosswordGeneratorComponent, WorkspaceComponent,
-  ContentReviewComponent, ContentStatusComponent } from './components';
+  ContentReviewComponent, ContentStatusComponent, HeaderComponent } from './components';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 
 import { PublicRoutingModule } from './public-routing.module';
-import {SuiDropdownModule, SuiDimmerModule} from 'ng2-semantic-ui';
+import {SuiDropdownModule, SuiDimmerModule, SuiPopupModule} from 'ng2-semantic-ui';
 
 @NgModule({
   imports: [
@@ -15,10 +15,13 @@ import {SuiDropdownModule, SuiDimmerModule} from 'ng2-semantic-ui';
     HttpClientModule,
     SuiDropdownModule,
     SuiDimmerModule,
-    FormsModule
+    FormsModule,
+    SuiPopupModule
   ],
   declarations: [LoginComponent, CrosswordGeneratorComponent, WorkspaceComponent,
     ContentReviewComponent,
-    ContentStatusComponent]
+    ContentStatusComponent,
+    HeaderComponent],
+  exports: [HeaderComponent]
 })
 export class PublicModule { }
